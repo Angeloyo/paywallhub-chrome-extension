8ac0047c0eb2bfec548f4773b88e451dbf649a63
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2023-06-04 16:13:13 +0200

    added functionality, and another provider

---
+++ js/popup.js
@@ -0,0 +1,44 @@
+
+function limpiarUrl(url) {
+  let urlObj = new URL(url);
+  return urlObj.origin + urlObj.pathname;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Botón 12ft
+  document.getElementById('boton-12ft').addEventListener('click', function() {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          let urlActual = tabs[0].url;
+          let urlLimpia = limpiarUrl(urlActual);
+          let urlDestino = 'https://12ft.io/' + urlLimpia;
+          chrome.tabs.create({ url: urlDestino });
+      });
+  });
+
+  document.getElementById('boton-internetarchive').addEventListener('click', function() {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          let urlActual = tabs[0].url;
+          let urlLimpia = limpiarUrl(urlActual);
+          let urlDestino = 'https://web.archive.org/' + urlLimpia;
+          chrome.tabs.create({ url: urlDestino });
+      });
+  });
+
+  document.getElementById('boton-archiveis').addEventListener('click', function() {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          let urlActual = tabs[0].url;
+          let urlLimpia = limpiarUrl(urlActual);
+          let urlDestino = 'https://archive.is/newest/' + urlLimpia;
+          chrome.tabs.create({ url: urlDestino });
+      });
+  });
+
+  document.getElementById('boton-removepaywall').addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        let urlActual = tabs[0].url;
+        let urlLimpia = limpiarUrl(urlActual);
+        let urlDestino = 'https://removepaywall.com/' + urlLimpia;
+        chrome.tabs.create({ url: urlDestino });
+    });
+});
+});
