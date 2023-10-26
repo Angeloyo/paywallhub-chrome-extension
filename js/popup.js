518971e57b4354ab12d9c7285e99671482f56939
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2023-10-26 22:53:54 +0200

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -54,7 +54,7 @@ document.addEventListener('DOMContentLoaded', function() {
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
           let urlActual = tabs[0].url;
           let urlLimpia = limpiarUrl(urlActual);
-          let urlDestino = 'https://archive.ph/newest/' + urlLimpia;
+          let urlDestino = 'https://archive.is/newest/' + urlLimpia;
           chrome.tabs.create({ url: urlDestino });
       });
   });
